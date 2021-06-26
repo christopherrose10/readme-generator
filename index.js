3307f91079a3b732f8f624c36491c6f244fc84be
UA CR <blazer5@email.arizona.edu>
2021-06-26 07:36:15 -0700

    build layout for readme

--- index.js
+++ index.js
@@ -1,34 +1,38 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
-var inquirer = require('inquirer');
-const generateMarkdown = require('./utils/generateMarkdown')
-const readmeContent = generateMarkdown;
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+const readmeContent = generateMarkdown(data);
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: 'input',
-        name: 'name',
-        message: 'What is your name?'
+        name: 'title',
+        message: 'What is the name of your project?'
     }
 ];
 
 const promptUser = () => {
-    return inquirer.prompt(questions);
+    return inquirer.prompt(questions)
+        .then(data => {
+
+
+            // const { message}
+
+            // // TODO: Create a function to write README file
+            fs.writeFile('README.md', readmeContent, err => {
+                if (err) throw err;
+                console.log('Portfolio complete! Check out index.html to see the output!');
+
+            });
+        });
 };
 
-// const { message}
-
-// // TODO: Create a function to write README file
-fs.writeFile('README.md', readmeContent, err => {
-    if (err) throw err;
-    console.log('Portfolio complete! Check out index.html to see the output!');
-
-});
 
 // // TODO: Create a function to initialize app
 // function init() { }
 
 // // Function call to initialize app
 // // init();
-promptUser()
-    .then(answers => console.log(answers));
+promptUser();
+
